41abe9b34fd800b0577e4ccb7a5e0100527f21e4
joaodettoni <joao.dettoni@maeztra.com>
2024-02-08 19:00:10 -0300

    feat: creating view

--- src/views/tag_creator_view.py
+++ src/views/tag_creator_view.py
@@ -1,13 +1,18 @@
 from src.views.http_types.http_request import HttpRequest
 from src.views.http_types.http_response import HttpResponse
-
+from src.controller.tag_creator_controller import TagCreatorController
 class TagCreatorView:
     '''
         responsability for interacting with HTTP
     '''
-    def validade_and_create_tag(self, http_request: HttpRequest):
+    def validade_and_create_tag(self, http_request: HttpRequest) -> HttpResponse:
+        tag_creator_controller = TagCreatorController()
+        
         body = http_request.body
         product_code = body["product_code"]
-        print(product_code)
+       
+        formatted_response = tag_creator_controller.create(product_code)
+        
+       
         #logica de regra de negocio
-        return HttpResponse(status_code=200, body={"tag_path": "path_from_tag"})
+        return HttpResponse(status_code=200, body=formatted_response)
