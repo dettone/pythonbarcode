29d6f8188605b201c37c4304b4454d8cf7e8333c
joaodettoni <joao.dettoni@maeztra.com>
2024-02-08 18:10:58 -0300

    feat: creating view

--- run_raw.py
+++ run_raw.py
@@ -8,6 +8,7 @@ app = Flask(__name__)
 def create_tag():
     body = request.json
     product_code = body.get('product_code')
+    
     tag = Code128(product_code, writer=ImageWriter())
     path_from_tag = f'{tag}'
     tag.save(path_from_tag)
---
+++ src/views/tag_creator_view.py
@@ -0,0 +1,13 @@
+from src.views.http_types.http_request import HttpRequest
+from src.views.http_types.http_response import HttpResponse
+
+class TagCreatorView:
+    '''
+        responsability for interacting with HTTP
+    '''
+    def validade_and_create_tag(self, http_request: HttpRequest):
+        body = http_request.body
+        product_code = body["product_code"]
+        print(product_code)
+        #logica de regra de negocio
+        return HttpResponse(status_code=200, body={"tag_path": "path_from_tag"})
